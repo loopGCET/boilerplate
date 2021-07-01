7ad98741016b0f710bffc4c9ff82937bec73da15
Abhinav Tiwari <abhinav2506293@gmail.com>
2021-07-01 23:43:52 +0530

    Update users.js

--- express/src/models/users.js
+++ express/src/models/users.js
@@ -1,4 +1,4 @@
-// this part should have the models based on what ORM you User. I am taking a constant for the sake of simplicity
+// this part should have the models based on what ORM you Use. I am taking a constant for the sake of simplicity
 const User = {
 	username: "abhinav1602",
 	name: "Abhinav Tiwari"
